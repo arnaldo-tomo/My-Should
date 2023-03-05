eba91f913eabb8c7084e544a6206a7b686b15974
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-03-05 12:44:09 +0200

    Update Router.jsx

--- Routers/Router.jsx
+++ Routers/Router.jsx
@@ -3,13 +3,12 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../Views/Home';
-import Should from '../Views/Should';
 const Router = () => {
     const Stack = createNativeStackNavigator();
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Home" component={Should} options={{ headerShown: false }} />
+                <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
     )
