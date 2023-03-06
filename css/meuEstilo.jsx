905c7f85b05b260e7c6789979d6b168c2bc164fa
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-03-06 13:33:48 +0200

    Create meuEstilo.jsx

---
+++ css/meuEstilo.jsx
@@ -0,0 +1,8 @@
+import { ScrollView, TouchableOpacity, Button, TextInput, View, StyleSheet } from 'react-native';
+
+const css4 = StyleSheet.create({
+    munhava: {
+        fontSize: 100,
+    }
+});
+export { css4 };
