ccd808494eaa4cce09ac988418abc53c0b8cbc53
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-03-11 19:06:24 +0200

    Update Home.jsx

--- Views/Home.jsx
+++ Views/Home.jsx
@@ -1,15 +1,15 @@
 import {
     NativeBaseProvider, Text, HStack, Heading, FlatList,
-    VStack, Spacer, Box, Input, FormControl, Fab, Icon, Switch, Actionsheet, Center, useDisclose, Path, Button
+    VStack, Spacer, Box, Input, Badge, Fab, Icon, Switch, Actionsheet, Center, useDisclose, Stack, Button
 } from 'native-base';
 import React, { useEffect, useState } from 'react';
 import { AntDesign, FontAwesome, MaterialIcons, Ionicons } from "@expo/vector-icons";
-import { ScrollView, TouchableOpacity, TextInput, View, StyleSheet } from 'react-native';
+import { ScrollView, TouchableOpacity, View, StyleSheet } from 'react-native';
 import axios from "axios";
 import { Formik } from 'formik';
 import { App, input } from '../config/App';
 import { login } from '../services/funcoes';
-const Home = () => {
+const Home = ({ navigation }) => {
     const {
         isOpen,
         onOpen,
@@ -21,13 +21,10 @@ const Home = () => {
         axios.get(App.APIURL + "GetAllShould")
 
             .then((response) => {
-                console.log(response.data.message)
                 SetGetAll(response.data.should)
 
             })
-            .catch((response) => {
-                console.log(response.data.message)
-            })
+
     }
 
     useEffect(() => {
@@ -43,7 +40,9 @@ const Home = () => {
         <NativeBaseProvider>
 
             <Actionsheet isOpen={isOpen} onClose={onClose} disableOverlay>
-                <Actionsheet.Content>
+                <Actionsheet.Content height={500} bgColor={'white'} _dragIndicator={{
+                    bg: 'blue.500'
+                }}>
 
                     <Formik initialValues={{ input }}
                         onSubmit={values => axios.post(App.APIURL + "POSTShouls", values)
@@ -53,7 +52,7 @@ const Home = () => {
                         } >
 
                         {({ handleChange, handleBlur, handleSubmit, values }) => (
-                            <Input />
+                            <></>
                         )}
                     </Formik>
 
@@ -64,23 +63,94 @@ const Home = () => {
                             Albums
                         </Text>
                     </Box>
-                    <Actionsheet.Item>Delete</Actionsheet.Item>
-                    <Actionsheet.Item>Share</Actionsheet.Item>
-                    <Actionsheet.Item>Play</Actionsheet.Item>
-                    <Actionsheet.Item>Favourite</Actionsheet.Item>
+
                     <Actionsheet.Item onPress={onClose}>Cancel</Actionsheet.Item>
                 </Actionsheet.Content>
             </Actionsheet>
-            <Heading paddingLeft={2} paddingTop={8} >
-                {App.nome}
-            </Heading>
+            <HStack justifyContent="space-between" p={4} pt={16} bgColor={'white'} >
+                <HStack>
+                    <VStack>
+                        <Heading fontWeight={'extraBlack'} >
+                            {App.nome}
+                        </Heading>
+                        <Text>{new Date().toLocaleString()}</Text>
+                    </VStack>
+
+                </HStack>
+
+                <HStack>
+                    <Heading  >
+                        <Ionicons name='calendar-outline' size={27} />
+                    </Heading>
+                </HStack>
+
+            </HStack>
+            <HStack p={2} bgColor={'white'}>
+                <Input placeholder="Procurar projectos" placeholderTextColor={'white'} variant="filled" shadow={6} color={'white'} bgColor={'blue.500'} width="100%" borderRadius="10" py="1" px="2" InputLeftElement={<Icon ml="2" size="4" color="white" as={<Ionicons name="ios-search" />} />} />
+            </HStack>
+
+            <HStack p={2} justifyContent={'space-between'} bgColor={'white'}>
+                <VStack>
+                    <Badge // bg="red.400"
+                        colorScheme="danger" rounded="full" mb={-4} mr={-4} zIndex={1} variant="solid" alignSelf="flex-end" _text={{
+                            fontSize: 12
+                        }}>
+                        5
+                    </Badge>
+                    <Button borderRadius="14" mx={{
+                        base: "auto",
+                        md: 0
+                    }} p="2" bg="blue.300" _text={{
+                        fontSize: 14,
+                        fontWeight: "bold"
+                    }}>
+                        Completos
+                    </Button>
+                </VStack>
+                <VStack>
+                    <Badge // bg="red.400"
+                        colorScheme="danger" rounded="full" mb={-4} mr={-4} zIndex={1} variant="solid" alignSelf="flex-end" _text={{
+                            fontSize: 12
+                        }}>
+                        2
+                    </Badge>
+                    <Button borderRadius="14" mx={{
+                        base: "auto",
+                        md: 0
+                    }} p="2" bg="cyan.500" _text={{
+                        fontSize: 14,
+                        fontWeight: "bold"
+                    }}>
+                        Pendentes
+                    </Button>
+                </VStack>
+                <VStack pr={2}>
+                    <Badge // bg="red.400"
+                        colorScheme="danger" rounded="full" mb={-4} mr={-4} zIndex={1} variant="solid" alignSelf="flex-end" _text={{
+                            fontSize: 12
+                        }}>
+                        2
+                    </Badge>
+                    <Button borderRadius="14" mx={{
+                        base: "auto",
+                        md: 0
+                    }} p="2" bg="cyan.500" _text={{
+                        fontSize: 14,
+                        fontWeight: "bold"
+                    }}>
+                        Em curso
+                    </Button>
+                </VStack>
+            </HStack>
 
 
-
-            <Fab renderInPortal={false} bottom={5} height={12} bgColor={'white'}
-                icon={<Icon color="black" as={AntDesign} name="plus" size="4" />} />
+            <Fab onPress={() => navigation.push('Post')} renderInPortal={false} bottom={5} bgColor={'white'}
+                icon={<Icon color="black" as={AntDesign} name="plus" size="8" />} bgcolor="black" />
             <ScrollView>
-                <View>
+
+                <View bgColor={'white'}>
+
+
                     <FlatList data={getAll} renderItem={({ item }) =>
 
 
@@ -88,17 +158,17 @@ const Home = () => {
                             <VStack p={2}  >
 
                                 <Box borderRadius={6} shadow={6} bgColor={'white'} py="2" >
-                                    <HStack p={2} fontWeight="400" alignItems="center" space={4}>
 
-                                        <Text p={2} fontWeight="400"  >
-                                            {item.should}
-                                        </Text>
-                                    </HStack>;
-                                    <Text fontSize="xs" _dark={{
-                                        color: "warmGray.50"
-                                    }} color="coolGray.800" alignSelf="flex-start">
-                                        <Switch size="sm" />
-                                    </Text>
+                                    <HStack p={1} fontWeight="400" alignItems="center" space={4}>
+                                        <Stack flex="1" p="2" justifyContent="space-around">
+                                            <Stack space="2">
+                                                <Heading size="md" ml="-1">
+                                                    {item.should}
+                                                </Heading>
+                                            </Stack>
+                                        </Stack>
+                                    </HStack>
+
                                 </Box>
                                 <Spacer />
                             </VStack>
