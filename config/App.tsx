da96a6864f1feb569bd6034c9a862487ea67e85b
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-03-13 00:24:08 +0200

    Update App.tsx

--- config/App.tsx
+++ config/App.tsx
@@ -1,12 +1,14 @@
 const App = {
     nome: 'MyShould',
-    APIURL: 'http://192.168.157.54:8000/api/',
+    APIURL: 'http://127.0.0.1:8000/api/',
     versao: 'V01',
 
 }
 
 const input = {
     inputShould: ' ',
+    lingauem: ' ',
+    descricao: ' ',
 }
 
 export { App, input };
