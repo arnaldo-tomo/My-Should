9b6b5051293ce1c411e1ddd77e958abca708bf3b
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-04-02 23:40:04 +0200

    Update Post.jsx

--- Views/Post.jsx
+++ Views/Post.jsx
@@ -1,4 +1,4 @@
-import { HStack, Image, Text, Stack, VStack, Input, Heading, Radio, TextArea, Box, Button, View, FormControl, Select, Center, CheckIcon, WarningOutlineIcon, NativeBaseProvider } from "native-base";
+import { HStack, Image, Text, Stack, VStack, Input, Heading, Radio, TextArea, Box, Button, View, Divider, Select, Center, CheckIcon, Actionsheet, NativeBaseProvider } from "native-base";
 import React from "react";
 import Ionic from '@expo/vector-icons';
 import axios from "axios";
@@ -7,13 +7,24 @@ import { GetData } from './Home';
 import { App, input } from "../config/App";
 import { StyleSheet } from 'react-native';
 
+
 const css = StyleSheet.create({ input: { with: 100, maxW: 350, mx: "auto", bgColor: "white" } })
 export default function Post({ navigation, route }) {
     //getting route params
     const { id, should, lingauem, descricao } = route.params;
+    const {
+        isOpen,
+        onOpen,
+        onClose
+    } = useDisclose();
     return (
         <NativeBaseProvider>
 
+            <Button onPress={onOpen}>Actionsheet</Button>
+
+            <Actionsheet isOpen={isOpen} onClose={onClose}>
+
+            </Actionsheet>
             <VStack bgcolor={'white'}>
                 <Box height={'50'} bgColor={'white'} borderTopRadius={"sm"} >
 
