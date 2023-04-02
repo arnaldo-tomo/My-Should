4f52daa8456f8b9043dc3219d545826a0cc225bf
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-04-02 17:49:43 +0200

    Create detalhes.tsx

---
+++ Views/detalhes.tsx
@@ -0,0 +1,17 @@
+import { NativeBaseProvider, Text, VStack, HStack, Actionsheet, Box, Center } from 'native-base';
+import React from "react";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const detaltes = () => {
+    return (
+        <NativeBaseProvider>
+            <Center>
+
+                <Box position={'absolute'} height={400} top={400} width={'full'} borderBottomColor={'blue.200'} borderTopRadius={30} bgColor={'white'}>
+                    hkhfjc,h
+                </Box>
+            </Center>
+        </NativeBaseProvider>
+    )
+}
+export default detaltes;
