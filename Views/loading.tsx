ada61434af802f80163e3dbc8952a5a1479850e1
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-04-04 15:12:17 +0200

    Update loading.tsx

--- Views/loading.tsx
+++ Views/loading.tsx
@@ -1,8 +1,35 @@
 import { NativeBaseProvider, Text, VStack, HStack, Heading, Box, Center, Button, Image, View } from 'native-base';
-import React from "react";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useEffect } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+
+// 
 
 const loading = ({ navigation }) => {
+
+
+
+    const fetchUserData = () => {
+        var nado = "logado";
+        const info = AsyncStorage.setItem('logado', nado);
+
+    }
+
+    useEffect(() => {
+        async function checkLoginStatus() {
+            const token = await AsyncStorage.getItem('logado');
+            if (token) {
+                // Enviar o token para o servidor para verificar se é válido
+                // Se o token for válido, redirecionar o usuário para a tela de Home
+                // Caso contrário, redirecionar o usuário para a tela de Login
+            }
+        }
+        checkLoginStatus();
+    }, []);
+
+
+
     return (
         <NativeBaseProvider>
 
@@ -20,7 +47,7 @@ const loading = ({ navigation }) => {
                     <Text style={{ textAlign: 'center', padding: 2 }}>O site Tenor.com foi traduzido com base na definição de idioma do navegador. Se quiser alterar o idioma</Text>
 
                 </Center>
-                <Button style={{ alignSelf: 'center' }} onPress={() => { navigation.navigate('Home') }}
+                <Button style={{ alignSelf: 'center' }} onPress={navigation.push('Home')}
                     position={'absolute'} bottom={50} shadow={'6'} width={"300"} bgColor={'blue.400'}>
                     Get-Start
                 </Button>
