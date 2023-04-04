f6d7350325216e89346421670d983c2493db9405
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-04-04 15:12:19 +0200

    Update Post.jsx

--- Views/Post.jsx
+++ Views/Post.jsx
@@ -5,26 +5,16 @@ import axios from "axios";
 import { Formik } from 'formik';
 import { GetData } from './Home';
 import { App, input } from "../config/App";
-import { StyleSheet } from 'react-native';
+import { StyleSheet, AsyncStorage } from 'react-native';
 
 
 const css = StyleSheet.create({ input: { with: 100, maxW: 350, mx: "auto", bgColor: "white" } })
 export default function Post({ navigation, route }) {
     //getting route params
     const { id, should, lingauem, descricao } = route.params;
-    const {
-        isOpen,
-        onOpen,
-        onClose
-    } = useDisclose();
+
     return (
         <NativeBaseProvider>
-
-            <Button onPress={onOpen}>Actionsheet</Button>
-
-            <Actionsheet isOpen={isOpen} onClose={onClose}>
-
-            </Actionsheet>
             <VStack bgcolor={'white'}>
                 <Box height={'50'} bgColor={'white'} borderTopRadius={"sm"} >
 
