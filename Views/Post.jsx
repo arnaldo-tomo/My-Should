bb3e858e64fa7141a176e3eb161f631e5cce9eda
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-04-05 12:05:49 +0200

    Update Post.jsx

--- Views/Post.jsx
+++ Views/Post.jsx
@@ -1,4 +1,4 @@
-import { HStack, Image, Text, Stack, VStack, Input, Heading, Radio, TextArea, Box, Button, View, Divider, Select, Center, CheckIcon, Actionsheet, NativeBaseProvider } from "native-base";
+import { HStack, Image, Text, Stack, VStack, Input, Heading, Radio, TextArea, Box, Button, View, Divider, Select, Center, CheckIcon, Actionsheet, NativeBaseProvider, useDisclose, } from "native-base";
 import React from "react";
 import Ionic from '@expo/vector-icons';
 import axios from "axios";
@@ -13,16 +13,44 @@ export default function Post({ navigation, route }) {
     //getting route params
     const { id, should, lingauem, descricao } = route.params;
 
+
+    function Example() {
+        const {
+            isOpen,
+            onOpen,
+            onClose
+        } = useDisclose();
+
+        useEffect(() => {
+
+        })
+        return <>
+            <Button onPress={onOpen}>Actionsheet</Button>
+
+            <Actionsheet isOpen={isOpen} onClose={onClose} disableOverlay>
+                <Actionsheet.Content>
+                    <Actionsheet.Item>Option 1</Actionsheet.Item>
+                    <Actionsheet.Item>Option 2</Actionsheet.Item>
+                    <Actionsheet.Item>Option 3</Actionsheet.Item>
+                </Actionsheet.Content>
+            </Actionsheet>
+        </>;
+    }
+
     return (
         <NativeBaseProvider>
             <VStack bgcolor={'white'}>
-                <Box bgColor={'white'} height="full" >
+                <Box bgColor={'white'} height="full" top={'full'} borderRadius={12} width={'2xl'} >
 
-                    <Heading padding={'2'}>{should}</Heading>
+                    <Heading style={{ textAlign: 'justify' }} padding={'2'}>{should}</Heading>
 
                 </Box>
 
             </VStack>
+
+            <Center flex={1} px="3">
+                <Example />
+            </Center>
         </NativeBaseProvider >
     )
 }
