d182c5cb447e24e5f720464f2d8692ff4f52d99b
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-04-05 12:05:47 +0200

    Update detalhes.tsx

--- Views/detalhes.tsx
+++ Views/detalhes.tsx
@@ -1,17 +1,22 @@
-// import { NativeBaseProvider, Text, VStack, HStack, Actionsheet, Box, Center } from 'native-base';
-// import React from "react";
+import { NativeBaseProvider, Text, VStack, HStack, Actionsheet, Box, Center, Container } from 'native-base';
+import React from "react";
 
 
-// const detaltes = () => {
-//     return (
-//         <NativeBaseProvider>
-//             <Center>
+const detaltes = () => {
+    return (
+        <NativeBaseProvider>
+            <Center>
 
-//                 <Box position={'absolute'} height={400} top={400} width={'full'} borderBottomColor={'blue.200'} borderTopRadius={30} bgColor={'white'}>
-//                     hkhfjc,h
-//                 </Box>
-//             </Center>
-//         </NativeBaseProvider>
-//     )
-// }
-// export default detaltes;
+                <Box top={400} width={'full'} borderBottomColor={'blue.200'} borderTopRadius={30} bgColor={'white'}>
+                    <Container>
+                        <Text p={2} textAlign={'justify'}>
+
+                            im hbvhjvljk ghh,jfkghkuvjhkh78888888888888888ytttt7 Download the React DevTools for a better development experience: Detalhes Download the React DevTools for a better development experience:
+                        </Text>
+                    </Container>
+                </Box>
+            </Center>
+        </NativeBaseProvider>
+    )
+}
+export default detaltes;
