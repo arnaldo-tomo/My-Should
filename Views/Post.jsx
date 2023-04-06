c87b67bf74b81bc217349caa35de3d15c825f7c4
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-04-06 11:23:08 +0200

    Update Post.jsx

--- Views/Post.jsx
+++ Views/Post.jsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text, Stack, VStack, Input, Heading, Radio, TextArea, Box, Button, View, Divider, Select, Center, CheckIcon, Actionsheet, NativeBaseProvider, useDisclose, } from "native-base";
 import React from "react";
-import Ionic from '@expo/vector-icons';
+import { HStack, Image, Text, Stack, VStack, Input, Heading, Radio, TextArea, Box, Button, View, Divider, Select, Center, CheckIcon, Actionsheet, NativeBaseProvider, useDisclose, } from "native-base";
 import axios from "axios";
 import { Formik } from 'formik';
 import { GetData } from './Home';
 import { App, input } from "../config/App";
 import { StyleSheet, AsyncStorage } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useEffect, useState } from 'react';
 
 
 const css = StyleSheet.create({ input: { with: 100, maxW: 350, mx: "auto", bgColor: "white" } })
@@ -13,34 +14,43 @@ export default function Post({ navigation, route }) {
     //getting route params
     const { id, should, lingauem, descricao } = route.params;
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    function onOpen() { setIsOpen(true); }
+
     function Example() {
-        const {
-            isOpen,
-            onOpen,
-            onClose
-        } = useDisclose();
+
+        const { isOpen, onOpen, onClose } = useDisclose();
+
+        useEffect(() => { onOpen(); }, []);
 
         return <>
-            {/* <Button onPress={onOpen}>Actionsheet</Button> */}
-
-            <Actionsheet isOpen={onOpen} onClose={onClose} disableOverlay>
+            <Actionsheet isOpen={isOpen} onClose={onClose} disableOverlay>
                 <Actionsheet.Content>
                     <Actionsheet.Item>{should}</Actionsheet.Item>
                     <Actionsheet.Item>Option 2</Actionsheet.Item>
-                    <Actionsheet.Item>Option 3</Actionsheet.Item>
-                    <Actionsheet.Item>Option 3</Actionsheet.Item>
-                    <Actionsheet.Item>Option 3</Actionsheet.Item>
-                    <Actionsheet.Item>Option 3</Actionsheet.Item>
-                    <Actionsheet.Item>Option 3</Actionsheet.Item>
-                    <Actionsheet.Item>Option 3</Actionsheet.Item>
+                    <Actionsheet.Item></Actionsheet.Item>
+                    <Actionsheet.Item></Actionsheet.Item>
+                    <Actionsheet.Item></Actionsheet.Item>
+                    <Actionsheet.Item></Actionsheet.Item>
+                    <Actionsheet.Item></Actionsheet.Item>
+                    <Actionsheet.Item></Actionsheet.Item>
+                    <Actionsheet.Item></Actionsheet.Item>
                 </Actionsheet.Content>
             </Actionsheet>
-        </>;
+        </>
     }
 
     return (
         <NativeBaseProvider>
-            <Center flex={1} px="2">
+            <Center flex={1} bgColor={'blue.100'}>
+                <Box w={200} shadow="10" position={'absolute'} bgColor={'white'} px="2" borderRadius={6}>
+                    <Text textAlign={'justify'} >
+                        {should}
+                    </Text>
+                </Box>
+            </Center>
+            <Center flex={1} px="2" bgColor={'blue.100'} >
                 <Example />
             </Center>
         </NativeBaseProvider >
